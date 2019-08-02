19139cf3b39ea0ee8e2b3f6bca5b8f9d7b3e0871
github username <github email address>
2019-08-02 10:56:23 -0400

    add length methods to developer and project models.

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,17 @@ class Project < ApplicationRecord
   has_many :jobs
   has_many :developers, through: :jobs
   belongs_to :user
+
+  def developers_skills_total
+    sum = 0
+    self.project.developers.each do |developer|
+      sum += developer.total_skills
+    end
+    return sum
+  end
+
+  # def project_length
+  #   if self.developers_skills_total
+  # end
+
 end
