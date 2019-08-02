2d4b34d079a1426deb2ad26ffc3f13cafaaab386
github username <github email address>
2019-08-02 11:52:05 -0400

    Revert "add project length, total skills to developer and project"
    This reverts commit 72a0103a986d93a4794f66649965e62dd43720f8.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,22 +7,14 @@ class Project < ApplicationRecord
 
   def developers_skills_total
     sum = 0
-    self.developers.each do |developer|
+    self.project.developers.each do |developer|
       sum += developer.total_skills
     end
     return sum
   end
 
-  def project_length
-    if self.developers_skills_total.between?(0,10)
-      return "This job will take about 2 months to complete"
-    elsif self.developers_skills_total.between?(10,30)
-      return "This job will take about 1 month to complete"
-    elsif self.developers_skills_total.between?(30,50)
-      return "This job will take about 3 weeks to complete"
-    elsif self.developers_skills_total > 50
-      return "This job will take 2 weeks or less to complete"
-    end
-  end
+  # def project_length
+  #   if self.developers_skills_total
+  # end
 
 end
